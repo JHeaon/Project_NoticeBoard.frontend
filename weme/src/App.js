cf5b1dc715620965d6827e225b873b9dc9ae030b
JHeaon <j3heawon@gmail.com>
2023-11-19 23:49:15 +0900

    Feat: Detail 페이지 생성

--- weme/src/App.js
+++ weme/src/App.js
@@ -2,6 +2,8 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Main from "./pages/Main";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
+import Post from "./pages/Post";
+import Detail from "./pages/Detail";
 
 function App(){
     return (
@@ -11,6 +13,8 @@ function App(){
                     <Route path="/" element={<Main />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<SignUp />} />
+                    <Route path="/post" element={<Post />} />
+                    <Route path="/detail" element={<Detail />} />
                 </Routes>
             </BrowserRouter>
         </div>
---
+++ weme/src/pages/Detail.js
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+
+const Detail = (props) => {
+    return (
+        <div>
+        <div className="flex flex-col  min-h-screen max-w-lg mx-auto bg-gray-100 pt-14 space-y-1">
+            <Header />
+        </div>
+        </div>
+
+    )
+}
+
+export default Detail;
